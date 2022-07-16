1b4ef42a12bdc632bb48f3d34f71f6fe0d3a98aa
luis machaca <luismachaca@outlook.com.pe>
2022-07-15 21:06:32 -0500

    updated PostList & added PostItem

---
+++ src/modules/journal/components/PostItem.vue
@@ -0,0 +1,54 @@
+<script setup></script>
+<template>
+  <div class="post text-sm font-medium cursor-pointer">
+    <div class="post-header">
+      <h3 class="post-title">Post</h3>
+      <div class="post-date">
+        <span class="mr-1.5">15</span>
+        <span class="mr-1.5">Julio</span>
+        <span>2021</span>
+      </div>
+    </div>
+    <div class="post-description">
+      <p class="text-left">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis
+        explicabo vitae sit quidem? Magnam, voluptatum nisi maxime soluta error
+        unde provident odio obcaecati sit optio quas nostrum nobis, consequatur
+        repellendus!
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped>
+.post {
+  margin: 0.5em;
+  background-color: hsl(215, 22%, 17%);
+  padding: 0.5em;
+  border-radius: 0.8em;
+  transition: all 0.2s ease-in;
+  &-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 0.8em;
+  }
+  &-title {
+    font-size: 0.9rem;
+    padding-left: 0.5em;
+    color: #79acdf;
+    text-transform: uppercase;
+  }
+  &-date {
+    outline: 2px solid #2c3e50;
+    border-radius: 1em;
+    padding: 0.2em 1em;
+  }
+  &-description {
+    border-left: 4px solid #2c3e50;
+    padding-left: 0.3em;
+  }
+  &:hover {
+    background-color: rgb(67, 87, 117);
+  }
+}
+</style>
--- src/modules/journal/components/PostList.vue
+++ src/modules/journal/components/PostList.vue
@@ -1,4 +1,9 @@
-<script setup></script>
+<script setup>
+import { defineAsyncComponent } from 'vue'
+const PostItem = defineAsyncComponent(() =>
+  import('../components/PostItem.vue')
+)
+</script>
 <template>
   <div class="list">
     <div class="px-2 pt-2">
@@ -8,11 +13,15 @@
         placeholder="search post"
       />
     </div>
+    <div class="item">
+      <post-item v-for="item in 100" :key="item" />
+    </div>
   </div>
 </template>
 <style scoped>
 .list {
-  height: 100%;
+  border-right: 1px solid #2c3e50;
+  height: calc(100vh - 50px);
 }
 .search {
   width: 100%;
@@ -20,5 +29,18 @@
   height: 35px;
   padding-left: 0.8em;
   border-radius: 8px;
+}
+.item {
+  height: calc(100vh - 150px);
+  overflow: auto;
+  &::-webkit-scrollbar {
+    width: 4px;
+    background-color: rgb(68, 68, 68);
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: rgb(116, 114, 114);
+  }
 }
 </style>
