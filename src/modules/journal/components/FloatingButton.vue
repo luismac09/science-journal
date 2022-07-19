1faff51dbd88c999da45b9750d582a89eecd6538
luis machaca <luismachaca@outlook.com.pe>
2022-07-18 20:14:50 -0500

    floating button was added  as a component

---
+++ src/modules/journal/components/FloatingButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+import addIcon from '@/assets/add.svg'
+</script>
+<template>
+  <button type="button" class="button">
+    <img
+      :src="addIcon"
+      alt="add icon"
+      title="add Post"
+      width="50"
+      height="50"
+    />
+  </button>
+</template>
+<style scoped>
+.button {
+  position: fixed;
+  bottom: 1.2em;
+  right: 1.5em;
+  & img {
+    border-radius: 50%;
+    &:hover {
+      background-color: #1d6042;
+    }
+    &:active {
+      opacity: 50%;
+    }
+  }
+}
+</style>
